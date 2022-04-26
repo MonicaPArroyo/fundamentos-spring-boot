9985a2804157e9e93ff80ac11cf7ee6a335e368e
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2022-04-26 15:57:28 -0500

    linea repetida eliminada

---
+++ src/main/java/com/fundamentos/springboot/fundamentosSP/FundamentosSpApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.fundamentosSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentos/springboot/fundamentosSP/FundamentosSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.fundamentosSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
